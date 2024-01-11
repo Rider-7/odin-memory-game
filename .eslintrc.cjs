15db0b502f3ea63e0ef56ae9c28b379dbf5c13f4
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2024-01-11 17:59:36 +0800

    initialise project

---
+++ .eslintrc.cjs
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+  ],
+  plugins: ['react', 'react-refresh', 'jsx-a11y', 'react-hooks', 'import'],
+  overrides: [
+    {
+      env: {
+        node: true,
+        browser: true,
+        es6: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: { react: { version: 'detect' } },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'linebreak-style': 0,
+  },
+};
